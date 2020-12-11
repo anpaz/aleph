e25d1ab2fbe9f88d41fb14631c8bc9d9a0c589aa
Andres Paz <anpaz@cs.washington.edu>
2020-12-10 20:35:06 -0700

    comments

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,7 +6,12 @@ import aleph.implicits._
 object Main extends App {
   println("Hello, aleph! ")
 
-  implicit val mode = json
+  implicit val mode = print
+
+  // Prepares a Qint at a specific value
+  val r0 = Qint.prepare(6)
+  println("* r0:")
+  println(r0)
 
   // Prepares a Qint in full super-position:
   val r1 = Qint.prepare()
@@ -18,20 +23,23 @@ object Main extends App {
   println("* r2:")
   println(r2)
 
-  // Simple oracle, 6 in
+  // Simple oracle, 3 in
   val o1 = (3 in r2)
   println(f"=== o1:\n${o1.eval}")
 
+  // Apply negation:
   val o2 = not(6 in r1)
   println(f"=== o2:\n${o2.eval}")
 
+  // Combine oracles using or
   val o3 = o1 or o2
   println(f"=== o3:\n${o3.eval}")
 
+  // More complex expression:
   val o4 = (6 in r1) and (12 in r1) or not(1 in r2)
   println(f"=== o4:\n${o4.eval}")
 
+  // Leverage `any` and `all`. They can be combined as any other oracle
   val o5 = (List(6, 12, 1) any r1) and (List(0, 1) all r2)
-  //val o5 =  (List(6, 12, 1) any r1)
   println(f"=== o5:\n${o5.eval}")
 }
