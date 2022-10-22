f1b0a4af53e59e990b2395a38fcf12904f0ddda3
Andres Paz <anpaz@cs.washington.edu>
2022-10-22 22:55:54 +0000

    pending test

--- src/tests/TestQPUQsharp.fs
+++ src/tests/TestQPUQsharp.fs
@@ -334,18 +334,19 @@ type TestQPUQsharp() =
             Tuple [ Int 0; Int 2; Int 0 ]
             Tuple [ Int 1; Int 1; Int 1 ]
             Tuple [ Int 1; Int 2; Int 2 ] ]
-          // // (k2, k2 = (k1 * k2))
-          // e.Join(
-          //     e.Var ("k2"),
-          //     e.Add(
-          //         e.Var ("k2"),
-          //         e.Multiply(e.Var "k1", e.Var "k2"))),
-          // [
-          //     Tuple [ Int 1; Int 1 ]
-          //     Tuple [ Int 2; Int 2 ]
-          //     Tuple [ Int 1; Int 2 ]
-          //     Tuple [ Int 2; Int 4 ]
-          // ]
+          // (k2, k2 == (k1 * k2))
+          e.Join(
+              e.Var ("k2"),
+              e.Join(
+                e.Var ("k1"),
+                e.Equals(
+                    e.Var ("k2"),
+                    e.Multiply(e.Var "k1", e.Var "k2")))),
+          [
+              Tuple [ Int 1; Int 0; Bool false ]
+              Tuple [ Int 1; Int 1; Bool true ]
+              Tuple [ Int 2; Int 0; Bool false ]
+              Tuple [ Int 2; Int 1; Bool true ] ]
           ]
         |> List.iter (verify_expression (prelude, this.QPU))
 
