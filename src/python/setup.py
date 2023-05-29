4a47118e288aaebf080450c44e11cba00827690d
Andres Paz <anpaz@cs.washington.edu>
2023-05-29 00:06:28 -0700

    fixing setup

--- src/python/setup.py
+++ src/python/setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/anpaz/aleph",
-    packages=setuptools.find_namespace_packages(include=["aleph-lang"]),
+    packages=setuptools.find_namespace_packages(include=["aleph_lang"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
