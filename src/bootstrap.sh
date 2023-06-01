1d3589d65cac97d3db772b6acfe3b7fac08332a6
Andres Paz <anpaz@cs.washington.edu>
2023-06-01 00:01:05 -0700

    Add azure functions to bootstrap; fix test. (#9)
    * adding extensions
    
    * fix test

--- src/bootstrap.sh
+++ src/bootstrap.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+# Install .net 6:
 curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin
+
+# Install azure functions cli
+npm i -g azure-functions-core-tools@4 --unsafe-perm true
+
+# assuming Python 3.9+ is already installed.
+dotnet --info
+echo Azure functions version: `func --version`
+python --version
