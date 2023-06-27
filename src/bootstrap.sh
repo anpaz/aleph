d530af97784410f109f72ce8c907f42f0cb45817
Andres Paz <anpaz@cs.washington.edu>
2023-06-27 00:10:32 -0700

    cron job to run once a week (#15)

--- src/bootstrap.sh
+++ src/bootstrap.sh
@@ -2,11 +2,21 @@
 
 # Install .net 6:
 curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin
+dotnet --info
 
 # Install azure functions cli
 npm i -g azure-functions-core-tools@4 --unsafe-perm true
 
 # assuming Python 3.9+ is already installed.
-dotnet --info
+
+# Print all versions
+echo 
+echo 
+echo ----------------------------------------------------------------
+echo .NET versions: 
+dotnet --list-sdks
+echo
 echo Azure functions version: `func --version`
+echo 
 python --version
+echo ----------------------------------------------------------------
